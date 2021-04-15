e4b5134b7becbbfa8cd522c7968b9e57b1828b78
Nathan <nathancai1031@gmail.com>
2021-04-15 17:57:18 -0400

    fixed bug in file sending

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.get("/api/config/paypal", (req, res) => {
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "/frontend/build", "index.html"));
+        res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
     });
 }
 
